3b69f354b308baa259dff597fb5a52610951b113
DanielS <dshlomo4@gmail.com>
2020-11-15 11:18:52 +0700

    Added out function

--- README.md
+++ README.md
@@ -6,12 +6,14 @@
 ```javascript
 
 const validate = require('arguments-validate')
-const t = new validate('C2 constructor')
 
-t.add('asd', 'string').minLength(5).maxLength(1).regex(/^[A-Za-z]+$/)
-t.add(1, 'number').min(2).is(x => x < 0)
-t.add({ header: 123 }, 'object').hasKey('header').nest({ header: { is: x => x < 0, type: 'string' } })
+function test() {
+    const v = new validate('Test function')
+    v.add(obj, 'object').hasKey('header').nest({ header: { type: 'object' } })
+        .add(cb, 'function', true) // Optional argument
+        .out((pre, errors) => console.error(pre, errors))
+    }
+}
 
-// Optional argument
-v.add(() => { }, 'object', true)
+test({body:123},'123')
 ```
--- index.js
+++ index.js
@@ -3,128 +3,128 @@ module.exports = function (name) {
     this.position = 0
     this.val = null
     this.type = null
-    this.prefix = 'ArgumentError in ' + name + ': Argument #'
-    this.utils = {
-        checkType: (val, type) => {
-            if (Array.isArray(val) && type == 'array') return true
-            if (typeof val != type) return false
-            return true
-        }
+    this.prefix = 'Argument error in `' + name + '`: Argument #'
+    this.stack = {};
+
+    this.checkType = (val, type) => {
+        if (Array.isArray(val) && type == 'array') return true
+        if (typeof val != type) return false
+        return true
     }
 
     this.add = (val, type, optional) => {
         this.val = val
         this.position++
+        this.stack[this.position] = []
         this.type = type
         if (optional) {
             if (val != undefined) {
-                if (!this.utils.checkType(this.val, type)) console.error(this.prefix + this.position + ' Expected type ' + type + ', got `' + typeof this.val + '`')
-
+                if (!this.checkType(this.val, type)) this.stack[this.position].push('Expected type ' + type + ', got `' + typeof this.val + '`')
             }
         } else {
-            if (!this.utils.checkType(this.val, type)) console.error(this.prefix + this.position + ' Expected type ' + type + ', got `' + typeof this.val + '`')
+            if (!this.checkType(this.val, type)) this.stack[this.position].push('Expected type ' + type + ', got `' + typeof this.val + '`')
         }
         return this
     }
 
 
     this.minLength = (minLength) => {
-        if (this.val.length < minLength) console.error(this.prefix + this.position + ' Expected ' + typeof this.val + ' to have a minimum length of `' + minLength + '`, got `' + this.val.length + '`')
+        if (this.val.length < minLength) this.stack[this.position].push('Expected ' + typeof this.val + ' to have a minimum length of `' + minLength + '`, got `' + this.val.length + '`')
         return this
     }
 
     this.nest_minLength = (key, minLength) => {
-        if (this.val[key].length < minLength) console.error(this.prefix + this.position + ' Expected key `' + key + '` inside object to have a minimum length of `' + minLength + '`, got `' + this.val[key].length + '`')
+        if (this.val[key].length < minLength) this.stack[this.position].push('Expected key `' + key + '` inside object to have a minimum length of `' + minLength + '`, got `' + this.val[key].length + '`')
         return this
     }
 
     this.maxLength = (maxLength) => {
-        if (this.val.length > maxLength) console.error(this.prefix + this.position + ' Expected ' + typeof this.val + ' to have a maximum length of `' + maxLength + '`, got `' + this.val.length + '`')
+        if (this.val.length > maxLength) this.stack[this.position].push('Expected ' + typeof this.val + ' to have a maximum length of `' + maxLength + '`, got `' + this.val.length + '`')
         return this
     }
 
     this.nest_maxLength = (key, maxLength) => {
-        if (this.val[key].length > maxLength) console.error(this.prefix + this.position + ' Expected key `' + key + '` inside object to have a maximum length of `' + maxLength + '`, got `' + this.val[key].length + '`')
+        if (this.val[key].length > maxLength) this.stack[this.position].push('Expected key `' + key + '` inside object to have a maximum length of `' + maxLength + '`, got `' + this.val[key].length + '`')
         return this
     }
 
     this.isLength = (length) => {
-        if (this.val.length != length) console.error(this.prefix + this.position + ' Expected ' + typeof this.val + ' to have a length of `' + length + '`, got `' + this.val.length + '`')
+        if (this.val.length != length) this.stack[this.position].push('Expected ' + typeof this.val + ' to have a length of `' + length + '`, got `' + this.val.length + '`')
         return this
     }
 
     this.nest_isLength = (key, length) => {
-        if (this.val[key].length != length) console.error(this.prefix + this.position + ' Expected key `' + key + '` inside object to have a length of `' + length + '`, got `' + this.val[key].length + '`')
+        if (this.val[key].length != length) this.stack[this.position].push('Expected key `' + key + '` inside object to have a length of `' + length + '`, got `' + this.val[key].length + '`')
         return this
     }
 
     this.regex = (regex) => {
-        if (!this.val.match(regex)) console.error(this.prefix + this.position + ' did not pass regex validation `' + regex + '`')
+        if (!this.val.match(regex)) this.stack[this.position].push('did not pass regex validation `' + regex + '`')
         return this
     }
 
     this.nest_regex = (key, regex) => {
-        if (!this.val[key].match(regex)) console.error(this.prefix + this.position + ' key `' + key + '` inside object did not pass regex validation `' + regex + '`')
+        if (!this.val[key].match(regex)) this.stack[this.position].push('key `' + key + '` inside object did not pass regex validation `' + regex + '`')
         return this
     }
 
     this.min = (min) => {
-        if (this.val < min) console.error(this.prefix + this.position + ' Expected ' + typeof this.val + ' to not be smaller than `' + min + '`, got `' + this.val + '`')
+        if (this.val < min) this.stack[this.position].push('Expected ' + typeof this.val + ' to not be smaller than `' + min + '`, got `' + this.val + '`')
         return this
     }
 
     this.nest_min = (key, min) => {
-        if (this.val[key] < min) console.error(this.prefix + this.position + ' Expected key `' + key + '` inside object to not be smaller than `' + min + '`, got `' + this.val[key] + '`')
+        if (this.val[key] < min) this.stack[this.position].push('Expected key `' + key + '` inside object to not be smaller than `' + min + '`, got `' + this.val[key] + '`')
         return this
     }
 
     this.max = (max) => {
-        if (this.val > max) console.error(this.prefix + this.position + ' Expected ' + typeof this.val + ' to not be larger than `' + max + '`, got `' + this.val + '`')
+        if (this.val > max) this.stack[this.position].push('Expected ' + typeof this.val + ' to not be larger than `' + max + '`, got `' + this.val + '`')
         return this
     }
 
     this.nest_max = (key, max) => {
-        if (this.val[key] > max) console.error(this.prefix + this.position + ' Expected key `' + key + '` inside object to not be larger than `' + max + '`, got `' + this.val[key] + '`')
+        if (this.val[key] > max) this.stack[this.position].push('Expected key `' + key + '` inside object to not be larger than `' + max + '`, got `' + this.val[key] + '`')
         return this
     }
 
     this.equal = (amount) => {
-        if (this.val != amount) console.error(this.prefix + this.position + ' Expected ' + typeof this.val + ' to be `' + amount + '`, got `' + this.val + '`')
+        if (this.val != amount) this.stack[this.position].push('Expected ' + typeof this.val + ' to be `' + amount + '`, got `' + this.val + '`')
         return this
     }
 
     this.nest_equal = (key, amount) => {
-        if (this.val[key] != amount) console.error(this.prefix + this.position + ' Expected key `' + key + '` inside object to be `' + amount + '`, got `' + this.val[key] + '`')
+        if (this.val[key] != amount) this.stack[this.position].push('Expected key `' + key + '` inside object to be `' + amount + '`, got `' + this.val[key] + '`')
         return this
     }
 
     this.hasKey = (key) => {
-        if (!this.val[key]) console.error(this.prefix + this.position + ' Expected ' + typeof this.val + ' to have key `' + key + '`')
+        if (!this.val[key]) this.stack[this.position].push('Expected ' + typeof this.val + ' to have key `' + key + '`')
         return this
     }
 
     this.nest_hasKey = (key, childKey) => {
-        if (!this.val[key][childKey]) console.error(this.prefix + this.position + ' Expected key `' + key + '` inside object to have key `' + childKey + '`')
+        if (!this.val[key][childKey]) this.stack[this.position].push('Expected key `' + key + '` inside object to have key `' + childKey + '`')
         return this
     }
 
     this.is = (fn) => {
-        if (!fn(this.val)) console.error(this.prefix + this.position + ' Expected ' + typeof this.val + ' to pass custom validation')
+        if (!fn(this.val)) this.stack[this.position].push('Expected ' + typeof this.val + ' to pass custom validation')
         return this
     }
 
     this.nest_is = (key, fn) => {
-        if (!fn(this.val[key])) console.error(this.prefix + this.position + ' Expected key `' + key + '` inside object to pass custom validation')
+        if (!fn(this.val[key])) this.stack[this.position].push('Expected key `' + key + '` inside object to pass custom validation')
         return this
     }
 
     this.type = (type) => {
-        if (!this.utils.checkType(this.val, type)) console.error(this.prefix + this.position + ' Expected type ' + type + ', got `' + typeof this.val + '`')
+        if (!this.checkType(this.val, type)) this.stack[this.position].push('Expected type ' + type + ', got `' + typeof this.val + '`')
         return this
     }
 
     this.nest_type = (key, type) => {
-        if (!this.utils.checkType(this.val[key], type)) console.error(this.prefix + this.position + ' Expected type of key `' + key + '` inside object to be ' + type + ', got `' + typeof this.val[key] + '`')
+        if (!this.checkType(this.val[key], type)) this.stack[this.position].push('Expected type of key `' + key + '` inside object to be ' + type + ', got `' + typeof this.val[key] + '`')
         return this
     }
 
@@ -133,6 +133,13 @@ module.exports = function (name) {
             for (const checkKey in object[key]) {
                 this['nest_' + checkKey](key, object[key][checkKey])
             }
+        }
+        return this
+    }
+
+    this.out = (cb) => {
+        for (const position in this.stack) {
+            if (this.stack[position].length > 0) cb(this.prefix + position, this.stack[position])
         }
     }
 }
